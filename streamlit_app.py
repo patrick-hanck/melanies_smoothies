33665c068c7527607318922d099e1fe386342fcf
patrick-hanck <61258192+patrick-hanck@users.noreply.github.com>
2025-04-26 21:42:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,9 @@ st.write(
 # input name
 input_name = st.text_input('Name on Smoothie')
 st.write("Name on Smoothie will be:",input_name)
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # multi-select fruit ingredients
 cnx = st.connection("snowflake")
